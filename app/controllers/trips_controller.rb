fd950f62c44895136a0fe6174b8e10b56ac4b9ef
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-09-02 13:46:26 +0900

    first commit

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,11 @@
 class TripsController < ApplicationController
   def index
   end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
