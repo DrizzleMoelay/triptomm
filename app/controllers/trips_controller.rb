69e390f5c6ac3a623d2a9ad9b421fdb65276fac2
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-09-02 13:56:53 +0900

    投稿機能を実装

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,6 +2,19 @@ class TripsController < ApplicationController
   def index
   end
 
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to root_path 
+    else
+      render :new
+    end
+  end
+
   private
 
   def trip_params
