081645754d201500360ec87ae1ac3883010e959a
DrizzleMoelay <moemoehtay2025@gmail.com>
2022-09-02 13:38:22 +0900

    バリデーションとアソシエーションの記述

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,11 @@
 class Trip < ApplicationRecord
+
+  validates :title, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+
+  belongs_to :user
+  has_one_attached :image
+  has_many :comments, dependent: :destroy
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
 validates :name, presence: true
 
-#has_many :trips
-#has_many :comments, dependent: :destroy
+has_many :trips
+has_many :comments, dependent: :destroy
 
 end
